fb925f0249c04259f15236021a36f88de86da0ab
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-03-15 08:55:28 -0400

    Better shell scripts

--- zsh.sh
+++ zsh.sh
@@ -3,11 +3,17 @@
 # need to install zsh 
 # also install neovim + vundle
 # also ohmyzsh + powerlevel10k
-# also jetbrains mono, and emojis
+# also jetbrains mono, and emojis - nerdfonts jetbrains font
 # also exa
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mkdir ~/nvim
+mv nvim.appimage ~/nvim/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sudo apt install exa -y
 
 sudo apt install zsh -y
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' > ~/.zshrc
