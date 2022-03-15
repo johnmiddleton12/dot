20ceee6f0946507d1a50725b2ad897c5be4f3fc5
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-03-15 08:57:26 -0400

    test

--- run.zsh
+++ run.zsh
@@ -5,6 +5,7 @@ if [ ! -d "~/.config" ]; then
 
 fi
 
+# get powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 rm ~/.p10k.zsh
