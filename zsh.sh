2e54b4f8f7bafa828df12324a90423cf669418a6
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-03-15 06:37:59 -0400

    new dot

---
+++ zsh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# need to install zsh 
+# also ohmyzsh + powerlevel10k
+# also jetbrains mono, and emojis
+# also exa
+
+sudo apt install zsh -y
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' > ~/.zshrc
